fd575d0d56510f9296efbd855dbebf19afaeb4e3
Ella Thompson <fang98t@gmail.com>
2021-02-08 14:30:03 -0500

    changing link

---
+++ build/precache-manifest.90a3daf0f131ef9e5e1e0f87b1850378.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35656d1a6778e3cc70fa76cbba8a16d0",
+    "url": "/reactPortfolio/index.html"
+  },
+  {
+    "revision": "66a0a3156470defbabbc",
+    "url": "/reactPortfolio/static/css/2.9a671a9a.chunk.css"
+  },
+  {
+    "revision": "60568a38a905580eadc2",
+    "url": "/reactPortfolio/static/css/main.76262749.chunk.css"
+  },
+  {
+    "revision": "66a0a3156470defbabbc",
+    "url": "/reactPortfolio/static/js/2.4d67733d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/reactPortfolio/static/js/2.4d67733d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60568a38a905580eadc2",
+    "url": "/reactPortfolio/static/js/main.69fe6b91.chunk.js"
+  },
+  {
+    "revision": "c21c98b102f83016f10c",
+    "url": "/reactPortfolio/static/js/runtime-main.1df8bede.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/reactPortfolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/reactPortfolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/reactPortfolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/reactPortfolio/static/media/slick.f97e3bbf.svg"
+  }
+]);
